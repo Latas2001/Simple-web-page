5833ad77230bd9c3f837744a9fb8095ede8b5380
Latas2001 <latasharmaqq@gmail.com>
2021-09-27 12:16:56 +0530

    first Commit

---
+++ css/style.c
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Best Online Food Delivery Services | myonlinemeals.com</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <nav id="navbar">
+        <div id="logo">
+            <img src="logo1.jpg" alt="myonlinemeals.com">
+        </div>
+        <ul>
+            <li class="item"><a href="#">Home</a></li>
+            <li class="item"><a href="#">Services</a></li>
+            <li class="item"><a href="#">About us</a></li>
+            <li class="item"><a href="#">Contact us</a></li>
+        </ul>
+    </nav>
+    <section id="Home">
+        <h1 class="h-primary">Welcome to MyOnlineMeal</h1>
+        <p>Lorem ipsum dolor sit amet consectetur,adipisicing elit.Perferendis dolorum
+            numquam minus.</p>
+        <p>Lorem ipsum dolor sit amet consectetur,
+            numquam minus. </p>
+        <button class="btn">Order Now</button>
+    </section>
+    <section class="services">
+        <h1 class="h-primary center">Our services</h1>
+        <div id="services">
+            
+			<!--<div class="image-wrapper">
+  <img class="image-1" src="img/mg.jpg"/>
+  <img class="image-2" src="img/mg.jpg" />
+  <img class="image-3" src="img/mg.jpg" />
+  <img class="image-4" src="img/mg.jpg" />
+</div>-->
+<div class="box">
+                <img src="img/mg.jpg" alt="">
+                <h2 class="h-secondary center">Food Ordering</h2>
+                <p class="center">Lorem ipsum dolor sit amet consectetur,adipisicing elit.Perferendis dolorum
+                    numquam minus.</p>
+            </div>
+            <div class="box">
+                <img src="img/fd.png" alt="">
+                <h2 class="h-secondary center">Food Catering</h2>
+                <p class="center">Lorem ipsum dolor sit amet consectetur,adipisicing elit.Perferendis dolorum
+                    numquam minus.</p>
+            </div>
+            <div class="box">
+                <img src="img/ham.jpg" alt="">
+                <h2 class="h-secondary center">Bulk Ordering</h2>
+                <p class="center">Lorem ipsum dolor sit amet consectetur,adipisicing elit.Perferendis dolorum
+                    numquam minus.</p>
+            </div>
+
+        </div>
+
+    </section>
+    <section id="client-section">
+
+
+        <h1 class="h-primary center">Our Clients</h1>
+        <div id="clients">
+            <div class="client-item">
+                <img src="img/apl.jpg" alt="Our Client">
+            </div>
+            <div class="client-item">
+                <img src="img/hp logo.jpg" alt="Our Client">
+            </div>
+            <div class="client-item">
+                <img src="img/skypelogo.jpg" alt="Our Client">
+            </div>
+            <div class="client-item">
+                <img src="img/micros.jpg" alt="Our Client">
+            </div>
+            <div class="client-item">
+                <img src="img/apl.jpg" alt="Our Client">
+            </div>
+        </div>
+    </section>
+    <section id="contact">
+        <h1 class="h-primary center">Contact us</h1>
+        <div id="contact box">
+            <form action="">
+                <div class="form-group">
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" placeholder="Enter your name">
+                
+                </div>
+
+            </form>
+        </div>
+    </section>
+</body>
+
+</html>
